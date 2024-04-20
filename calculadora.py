791d87ee3f0c0629c948a64560e1a62471521f57
Paul0breg <167637122+Paul0breg@users.noreply.github.com>
2024-04-20 13:28:56 -0300

    paulo obregon
    missao pratica nivel 1

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+print("missão pratica")
+print("calculadora")
+print("aluno: paulo obregon")
+a=int(input("insira o primeiro número: "))
+b=int(input("insira o segundo número: "))
+soma=a+b
+print("resultado da soma: " + str(soma))
+
+sub= a-b  
+print("resultado da subtração: "+str(sub))
+
+mult= a*b
+print("resultado da multiplicação: "+str(mult))
+
+div= a/b 
+print("resultado da divisão: "+str(div))
+
+
+
